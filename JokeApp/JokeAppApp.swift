cb98c0867089bccc466bd17f1080ff74f6273e62
Mohamed Amr <mohamedamr9722@gmail.com>
2025-10-20 18:45:48 +0300

    Initial Commit

---
+++ JokeApp/JokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAppApp.swift
+//  JokeApp
+//
+//  Created by Mohamed Amr on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct JokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
